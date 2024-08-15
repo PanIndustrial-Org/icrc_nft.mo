c1f6efd9970a15690a0060deaa702e43efea468c
panindustrial-dev <info@panindustrial.com>
2024-08-15 14:15:24 -0500

    fix for https://github.com/PanIndustrial-Org/icrc_nft.mo/issues/3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+
+# v0.2.2
+
+Update mops for ICRC-7 and ICRC-37 to fix token_of bug https://github.com/PanIndustrial-Org/icrc_nft.mo/issues/3
+
+
 # v0.1.1
 
 Update mops
--- example/deploy.sh
+++ example/deploy.sh
@@ -50,14 +50,18 @@ dfx canister call icrc7 icrcX_mint "(
       token_id = 0 : nat;
       owner = opt record { owner = principal \"$ICRC7_CANISTER\"; subaccount = null;};
       metadata = variant {
-        Class = vec {
-          record {
-            value = variant {
-              Text = \"https://images-assets.nasa.gov/image/PIA18249/PIA18249~orig.jpg\"
-            };
-            name = \"icrc7:metadata:uri:image\";
-            immutable = true;
-          };
+        Map = vec {
+          record { \"icrc97:metadata\"; variant { Map = vec {
+            record { \"name\"; variant { Text = \"Image 1\" } };
+            record { \"description\"; variant { Text = \"A beautiful space image from NASA.\" } };
+            record { \"assets\"; variant { Array = vec {
+              variant { Map = vec {
+                record { \"url\"; variant { Text = \"https://images-assets.nasa.gov/image/PIA18249/PIA18249~orig.jpg\" } };
+                record { \"mime\"; variant { Text = \"image/jpeg\" } };
+                record { \"purpose\"; variant { Text = \"icrc97:image\" } }
+              }}
+            }}}
+          }}}
         }
       };
       memo = opt blob \"\00\01\";
@@ -68,14 +72,18 @@ dfx canister call icrc7 icrcX_mint "(
       token_id = 1 : nat;
       owner = opt record { owner = principal \"$ICRC7_CANISTER\"; subaccount = null;};
       metadata = variant {
-        Class = vec {
-          record {
-            value = variant {
-              Text = \"https://images-assets.nasa.gov/image/GSFC_20171208_Archive_e001465/GSFC_20171208_Archive_e001465~orig.jpg\"
-            };
-            name = \"icrc7:metadata:uri:image\";
-            immutable = true;
-          };
+        Map = vec {
+          record { \"icrc97:metadata\"; variant { Map = vec {
+            record { \"name\"; variant { Text = \"Image 2\" }};
+            record { \"description\"; variant { Text = \"Another stunning NASA image.\" } };
+            record { \"assets\"; variant { Array = vec {
+              variant { Map = vec {
+                record { \"url\"; variant { Text = \"https://images-assets.nasa.gov/image/GSFC_20171208_Archive_e001465/GSFC_20171208_Archive_e001465~orig.jpg\" } };
+                record { \"mime\"; variant { Text = \"image/jpeg\" } };
+                record { \"purpose\"; variant { Text = \"icrc97:image\" } }
+              }}
+            }}}
+          }}}
         }
       };
       memo = opt blob \"\00\01\";
@@ -86,14 +94,18 @@ dfx canister call icrc7 icrcX_mint "(
       token_id = 2 : nat;
       owner = opt record { owner = principal \"$ICRC7_CANISTER\"; subaccount = null;};
       metadata = variant {
-        Class = vec {
-          record {
-            value = variant {
-              Text = \"https://images-assets.nasa.gov/image/hubble-sees-the-wings-of-a-butterfly-the-twin-jet-nebula_20283986193_o/hubble-sees-the-wings-of-a-butterfly-the-twin-jet-nebula_20283986193_o~orig.jpg\"
-            };
-            name = \"icrc7:metadata:uri:image\";
-            immutable = true;
-          };
+        Map = vec {
+          record { \"icrc97:metadata\"; variant { Map = vec {
+            record { \"name\"; variant { Text = \"Image 3\" } };
+            record { \"description\"; variant { Text = \"Hubble sees the wings of a butterfly.\" } };
+            record { \"assets\"; variant { Array = vec {
+              variant { Map = vec {
+                record { \"url\"; variant { Text = \"https://images-assets.nasa.gov/image/hubble-sees-the-wings-of-a-butterfly-the-twin-jet-nebula_20283986193_o/hubble-sees-the-wings-of-a-butterfly-the-twin-jet-nebula_20283986193_o~orig.jpg\" } };
+                record { \"mime\"; variant { Text = \"image/jpeg\" } };
+                record { \"purpose\"; variant { Text = \"icrc97:image\" } }
+              }}
+            }}}
+          }}}
         }
       };
       memo = opt blob \"\00\01\";
@@ -104,21 +116,25 @@ dfx canister call icrc7 icrcX_mint "(
       token_id = 3 : nat;
       owner = opt record { owner = principal \"$ICRC7_CANISTER\"; subaccount = null;};
       metadata = variant {
-        Class = vec {
-          record {
-            value = variant {
-              Text = \"https://images-assets.nasa.gov/image/GSFC_20171208_Archive_e001518/GSFC_20171208_Archive_e001518~orig.jpg\"
-            };
-            name = \"icrc7:metadata:uri:image\";
-            immutable = true;
-          };
+        Map = vec {
+          record { \"icrc97:metadata\"; variant { Map = vec {
+            record { \"name\"; variant { Text = \"Image 4\" } };
+            record { \"description\"; variant { Text = \"Another beautiful image from NASA archives.\" } };
+            record { \"assets\"; variant { Array = vec {
+              variant { Map = vec {
+                record { \"url\"; variant { Text = \"https://images-assets.nasa.gov/image/GSFC_20171208_Archive_e001518/GSFC_20171208_Archive_e001518~orig.jpg\" } };
+                record { \"mime\"; variant { Text = \"image/jpeg\" } };
+                record { \"purpose\"; variant { Text = \"icrc97:image\" } }
+              }}
+            }}}
+          }}}
         }
       };
       memo = opt blob \"\00\01\";
       override = true;
       created_at_time = null;
     };
-  },
+  }
 )"
 
 # Get total supply
